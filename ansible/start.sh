921f23d9de3650c718a77f9153afe25cfbaa2d6e
dev <dev@haruhi.tail76174f.ts.net>
2024-08-06 19:54:35 +0200

    updating ansible configurations

--- ansible/start.sh
+++
@@ -1,2 +0,0 @@
-# Only a test file lol.
-ansible-playbook -i inventory.yaml playbook.yaml --connection=local
--- nixos/nix.modern14.sh
+++
@@ -1,3 +0,0 @@
-dbus-uuidgen --ensure=/etc/machine-id
-nixos-rebuild switch --flake .#x86_64-dev-modern14
-
