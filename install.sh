8c5a13fc13b4a0e49de16fe8eefd60b4d4c46e28
akrck02 <aketza.vazquez@gmail.com>
2025-05-11 20:24:05 +0200

    install: options on script

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@ select lng in slimbook-executive-16 fuyu yoga Quit
 do
     case $lng in
         "slimbook-executive-16")
+        	sh ./deploy.sh
             echo "Installing nixos on host $lng"
             nixos-rebuild switch --flake /etc/nixos/#slimbook-executive-16 ;;
         "fuyu")
+        	sh ./deploy.sh
         	echo "Installing nixos on host $lng"
          	nixos-rebuild switch --flake /etc/nixos/#fuyu ;;
         "yoga")
+        	sh ./deploy.sh
             echo "Installing nixos on host $lng"
             nixos-rebuild switch --flake /etc/nixos/#yoga ;;
         "Quit")
