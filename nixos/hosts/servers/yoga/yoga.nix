771f4606a3c636e4ad1183c1fa3d757053153394
akrck02 <aketza.vazquez@gmail.com>
2025-05-11 20:10:19 +0200

    yoga: hardware

---
+++ nixos/hosts/servers/yoga/yoga.nix
@@ -0,0 +1,39 @@
+# Slimbook executive 16 custom configuration
+{ pkgs, lib, config, inputs, home-manager, ...} : {
+
+
+
+  ## Configuration
+  config = {
+
+    ## System state version
+    system.stateVersion = "24.11";
+
+    # Allow experimental features
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Allow unfree software
+    nixpkgs.config.allowUnfree = true;
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    # Hostname
+    networking.hostName = "yoga";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Exclude xterm
+    services.xserver.excludePackages = [pkgs.xterm];
+
+  };
+
+  ## Modules to import
+  imports = [
+    ./hardware.nix
+    ./power-management.nix
+    ../../../profiles/low-power-server.nix
+  ];
+}
