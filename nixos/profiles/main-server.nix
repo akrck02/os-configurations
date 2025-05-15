30922c2e9673dcc21d41307ca24be09f8cda4799
akrck02 <aketza.vazquez@gmail.com>
2025-05-15 20:04:31 +0200

    fuyu: config base

---
+++ nixos/profiles/main-server.nix
@@ -0,0 +1,33 @@
+{ pkgs, inputs, ...} : {
+
+ ## Modules to import
+  imports = [
+    ../modules/nixos/languages/english-es.nix
+    ../modules/nixos/cli/cli.nix
+    ../modules/nixos/networking/networking.nix
+    ../modules/nixos/containers/docker.nix
+    ../modules/nixos/development/language.servers.nix
+  ];
+
+  ## Configuration
+  config = {
+
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.extraSpecialArgs = {inherit inputs;};
+	  home-manager.backupFileExtension = "back";
+
+		# Development user
+    users.users.yoga = {
+        isNormalUser = true;
+        home = "/home/fuyu";
+        initialPassword = "12345";
+        description = "Admin user for fuyu server";
+        shell = pkgs.zsh;
+        createHome = true;
+        extraGroups = [ "networkmanager" "wheel" "docker" ];
+        openssh.authorizedKeys.keys = [];
+    };
+    home-manager.users.yoga = import ../users/fuyu.nix;
+
+  };
+}
---
+++ nixos/users/fuyu.nix
@@ -0,0 +1,34 @@
+ { pkgs, ... }: {
+
+   ## Modules to import
+   imports = [
+
+     ## Development apps
+     ../modules/home-manager/development/code/neovim.nix
+
+     ../modules/home-manager/development/languages/python313.nix
+     ../modules/home-manager/development/languages/typescript.nix
+
+   ];
+
+   config = {
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "24.11";
+
+    # Enable git
+    programs.git = {
+        enable = true;
+        userName = "akrck02";
+        userEmail = "aketza.vazquez@gmail.com";
+    };
+
+    ## Neovim
+    home.file.".config/nvim" = {
+        source = ../dotfiles/.nvim;
+        recursive = true;
+    };
+
+   };
+}
