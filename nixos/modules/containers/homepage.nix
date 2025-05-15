1b2b0cdaa0356c7f91b12b758c5bd0d81842ecdc
akrck02 <aketza.vazquez@gmail.com>
2025-05-15 23:49:05 +0200

    container: homepage

---
+++ nixos/modules/containers/homepage.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+{
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      Homepage = {
+        autoStart = true;
+        image = "ghcr.io/gethomepage/homepage:latest";
+        extraOptions = [ "--net=host" ];
+        ports = [
+          "3000:3000"
+        ];
+        volumes = [
+          "/home/fuyu/.dotfiles/Homepage-startpage/config:/app/config"
+          "/home/fuyu/.dotfiles/Homepage-startpage/Graphics/icons:/app/public/icons"
+        ];
+      };
+    };
+  };
+}
