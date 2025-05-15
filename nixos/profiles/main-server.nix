3f01ab2fcbb3dbca0eb93dc1f595ac9b971a4e17
akrck02 <aketza.vazquez@gmail.com>
2025-05-15 23:50:48 +0200

    container: homepage

--- nixos/profiles/main-server.nix
+++ nixos/profiles/main-server.nix
@@ -7,7 +7,7 @@
     ../modules/nixos/networking/networking.nix
     ../modules/nixos/containers/docker.nix
     ../modules/nixos/development/language.servers.nix
-    ../modules/containers/portainer.nix
+    ../modules/containers/hompage.nix
   ];
 
   ## Configuration
