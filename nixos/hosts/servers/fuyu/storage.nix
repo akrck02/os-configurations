cfc5bdc0bceef41d98094bc989e25ba3f4d2e385
akrck02 <aketza.vazquez@gmail.com>
2025-05-17 20:15:00 +0200

    fuyu: disks

--- nixos/hosts/servers/fuyu/storage.nix
+++ nixos/hosts/servers/fuyu/storage.nix
@@ -2,16 +2,14 @@
 { pkgs, lib, config, inputs, home-manager, ...} : {
 
 
-  # fileSystems."/media/services" = {
-  # 	device = "/dev/disk/by-uuid/159b1095-bd53-45e7-9701-5cabfa4908c5";
-  #   fsType = "ext4";
-  #   options = [ "fmask=0077" "dmask=0077" ];
-  # };
+  fileSystems."/media/services" = {
+  	device = "/dev/disk/by-uuid/159b1095-bd53-45e7-9701-5cabfa4908c5";
+    fsType = "ext4";
+  };
 
   # fileSystems."/media/backup" = {
   # 	device = "/dev/disk/by-uuid/44c7a18b-3c9a-49f5-a800-85a329490a7a";
   #   fsType = "ext4";
-  #   options = [ "fmask=0077" "dmask=0077" ];
   # };
 
 
