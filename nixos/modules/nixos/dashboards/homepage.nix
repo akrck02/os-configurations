bbc0a8f3836937b1807332ea0e2526c441f502f0
akrck02 <aketza.vazquez@gmail.com>
2025-05-17 18:48:28 +0200

    homepage: changes

--- nixos/modules/nixos/dashboards/homepage.nix
+++ nixos/modules/nixos/dashboards/homepage.nix
@@ -133,11 +133,15 @@
 		];
 
 	  widgets = [
-      {
+			{
+				{
 
-      	logo = {
+					logo = {
        		icon = "https://avatars.githubusercontent.com/u/43274508?v=4";
        	};
+			}
+      {
+
       	resources = {
       		label = "system";
         	cpu = true;
