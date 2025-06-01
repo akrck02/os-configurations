183bab9c3fdb5d1f807c52d6c108fb9c5f640334
akrck02 <aketza.vazquez@gmail.com>
2025-06-01 17:42:11 +0200

    portainer tweaks

--- nixos/modules/containers/portainer.nix
+++ nixos/modules/containers/portainer.nix
@@ -12,9 +12,9 @@
 	     	portainer = {
 	       image = "docker.io/portainer/portainer-ce:2.29.2-alpine";
 	       ports = [
-	       	"127.0.0.1:9000:9000"
-	        "127.0.0.1:9443:9443"
-	        "127.0.0.1:8000:8000"
+	       	"0.0.0.0:9000:9000"
+	        "0.0.0.0:9443:9443"
+	        "0.0.0.0:8000:8000"
 	       ];
 	       volumes = [
 	         "/root/portainer:/data"
