bb1086aa800caa395b5516fc7c5ff6afcec8e786
akrck02 <aketza.vazquez@gmail.com>
2025-06-01 11:12:54 +0200

    fake password wip

--- nixos/profiles/main-server.nix
+++ nixos/profiles/main-server.nix
@@ -12,7 +12,7 @@
     # services
     ../modules/nixos/services/homepage/homepage.nix
     ../modules/nixos/services/metrics/server-metrics.nix
-    ../modules/nixos/services/cloud/nextcloud.nix
+    #../modules/nixos/services/cloud/nextcloud.nix
   ];
 
   ## Configuration
