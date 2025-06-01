74dbae3df069f38d7e085ecbf7b071aa112aaa5e
akrck02 <aketza.vazquez@gmail.com>
2025-06-01 17:37:11 +0200

    portainer tweaks

--- nixos/modules/containers/portainer.nix
+++ nixos/modules/containers/portainer.nix
@@ -6,19 +6,21 @@
 	   trustedInterfaces = [ "enp1s0" ];
 	 };
 
-   virtualisation.oci-containers.containers = {
-   		backend = "docker";
-     	portainer = {
-       image = "docker.io/portainer/portainer-ce:2.29.2-alpine";
-       ports = [
-       	"127.0.0.1:9000:9000"
-        "127.0.0.1:9443:9443"
-        "127.0.0.1:8000:8000"
-       ];
-       volumes = [
-         "/root/portainer:/data"
-       ];
-       cmd = [];
-     };
+   virtualisation.oci-containers = {
+   	backend = "docker";
+   	containers = {
+	     	portainer = {
+	       image = "docker.io/portainer/portainer-ce:2.29.2-alpine";
+	       ports = [
+	       	"127.0.0.1:9000:9000"
+	        "127.0.0.1:9443:9443"
+	        "127.0.0.1:8000:8000"
+	       ];
+	       volumes = [
+	         "/root/portainer:/data"
+	       ];
+	       cmd = [];
+	     };
+	   };
    };
  }
