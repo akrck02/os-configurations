5015c7c68b5aee58cf3c83e65be31f4879985e4e
akrck02 <aketza.vazquez@gmail.com>
2025-06-01 12:43:42 +0200

    nextcloud tweaks

--- nixos/modules/nixos/services/cloud/nextcloud.nix
+++ nixos/modules/nixos/services/cloud/nextcloud.nix
@@ -1,50 +1,62 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs ... }:
 {
-  services = {
+
+	environment.etc."nextcloud-admin-pass".text = "PWD";
+	services.nextcloud = {
+	  enable = true;
+	  package = pkgs.nextcloud31;
+	  hostName = "localhost";
+	  config.adminpassFile = "/etc/nextcloud-admin-pass";
+	  config.dbtype = "sqlite";
+	};
+
+  #services = {
 
     ## Nextcloud
-    nextcloud = {
-      enable = true;
-      package = pkgs.nextcloud31;
-      autoUpdateApps.enable = true;
-      config = {
-        dbtype = "pgsql";
-        dbuser = "nextcloud";
-        dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-        dbname = "nextcloud";
+  #  nextcloud = {
+  #    enable = true;
+  #    package = pkgs.nextcloud31;
+  #    autoUpdateApps.enable = true;
+  #    config = {
+  #      dbtype = "pgsql";
+  #      dbuser = "nextcloud";
+  #      dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
+  #      dbname = "nextcloud";
 
-        trustedProxies = [
-          "localhost"
-          "127.0.0.1"
-        ];
-        extraTrustedDomains = [ ];
-      };
+  #      trustedProxies = [
+  #        "localhost"
+  #        "127.0.0.1"
+  #      ];
+  #      extraTrustedDomains = [ ];
+  #    };
 
       # specify only if you want redis caching
-      extraOptions = {};
-    };
+  #    extraOptions = {};
+  #  };
 
     ## PostgreSQL
-    postgresql = {
-      enable = true;
-      ensureDatabases = [ "nextcloud" ];
-    };
+  #  postgresql = {
+  #    enable = true;
+  #    ensureDatabases = [ "nextcloud" ];
+  #  };
 
     # optional backup for postgresql db
-    postgresqlBackup = {
-      enable = true;
-      databases = [ "nextcloud" ];
-    };
-  };
+  #  postgresqlBackup = {
+  #    enable = true;
+  #    databases = [ "nextcloud" ];
+  #  };
+  #};
 
-  systemd.services.postgresql.postStart = lib.mkAfter ''
-    $PSQL mydb -tAc 'GRANT ALL ON ALL TABLES IN SCHEMA public TO nextcloud' || true
-    $PSQL mydb -tAc 'GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nextcloud' || true
-  '';
+  #systemd.services.postgresql.postStart = lib.mkAfter ''
+  #  $PSQL mydb -tAc 'GRANT ALL ON ALL TABLES IN SCHEMA public TO nextcloud' || true
+  #  $PSQL mydb -tAc 'GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nextcloud' || true
+  #'';
 
   # ensure that postgres is running *before* running the setup
-  systemd.services."nextcloud-setup" = {
-    requires = [ "postgresql.service" ];
-    after = [ "postgresql.service" ];
-  };
+  #systemd.services."nextcloud-setup" = {
+  #  requires = [ "postgresql.service" ];
+  #  after = [ "postgresql.service" ];
+  #};
+
+
 }
