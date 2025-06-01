347dd4ce9d2a4dde9c58564260560d7c0afb08e4
akrck02 <aketza.vazquez@gmail.com>
2025-06-01 20:14:06 +0200

    nginx

--- nixos/profiles/main-server.nix
+++ nixos/profiles/main-server.nix
@@ -60,45 +60,34 @@
     ## Nginx reverse proxy
     services.nginx = {
       enable = true;
-#      recommendedProxySettings = true;
-#      recommendedTlsSettings = true;
 
       # other Nginx options
-      virtualHosts.localhost = {
-#        enableACME = false;
-#        forceSSL = false;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8082";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-          	# required when the target is also TLS server with multiple hosts
-           "proxy_ssl_server_name on;"
-           +
-           # required when the server wants to use HTTP Authentication
-           "proxy_pass_header Authorization;";
-        };
+      virtualHosts = {
+       	localhost = {
+	        locations."/" = {
+	          proxyPass = "http://127.0.0.1:8082";
+	          proxyWebsockets = true; # needed if you need to use WebSocket
+	          extraConfig =
+	          	# required when the target is also TLS server with multiple hosts
+	           "proxy_ssl_server_name on;"
+	           +
+	           # required when the server wants to use HTTP Authentication
+	           "proxy_pass_header Authorization;";
+	        };
+	      };
 
-        locations."/nextcloud/" = {
-          proxyPass = "http://127.0.0.1:8009";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-          	# required when the target is also TLS server with multiple hosts
-           "proxy_ssl_server_name on;"
-           +
-           # required when the server wants to use HTTP Authentication
-           "proxy_pass_header Authorization;";
-        };
-
-        locations."/grafana/" = {
-          proxyPass = "http://127.0.0.1:3000";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-          	# required when the target is also TLS server with multiple hosts
-           "proxy_ssl_server_name on;"
-           +
-           # required when the server wants to use HTTP Authentication
-           "proxy_pass_header Authorization;";
-        };
+		  	grafana.localhost = {
+		      locations."/grafana/" = {
+		        proxyPass = "http://127.0.0.1:3000";
+		        proxyWebsockets = true; # needed if you need to use WebSocket
+		        extraConfig =
+		        	# required when the target is also TLS server with multiple hosts
+		         "proxy_ssl_server_name on;"
+		         +
+		         # required when the server wants to use HTTP Authentication
+		         "proxy_pass_header Authorization;";
+		      };
+				};
       };
     };
 
