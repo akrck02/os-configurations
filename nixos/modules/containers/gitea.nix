eca40603b19c18fa66b4c56c6821cd9b1787fa1f
akrck02 <aketza.vazquez@gmail.com>
2025-06-02 20:58:48 +0200

    Update gitea.nix

--- nixos/modules/containers/gitea.nix
+++ nixos/modules/containers/gitea.nix
@@ -3,26 +3,26 @@
 {
 
   # Gitea
-  virtualisation.oci-containers.containers = {
+  virtualisation.oci-containers = {
   	backend = "docker";
-
-   	"gitea" = {
-	    autoStart = true;
-	    image = "docker.io/gitea/gitea:latest";
-	    environment = {
-	      USER_UID = "1000";
-	      USER_GID = "1000";
-	    };
-	    volumes = [
-	      "/media/services/development/gtea:/data"
-	      "/etc/timezone:/etc/timezone:ro"
-	      "/etc/localtime:/etc/localtime:ro"
-	    ];
-	    ports = [
-	      "3000:3000"
-	      "222:22"
-	    ];
+	  containers = {
+	   	"gitea" = {
+		    autoStart = true;
+		    image = "docker.io/gitea/gitea:latest";
+		    environment = {
+		      USER_UID = "1000";
+		      USER_GID = "1000";
+		    };
+		    volumes = [
+		      "/media/services/development/gtea:/data"
+		      "/etc/timezone:/etc/timezone:ro"
+		      "/etc/localtime:/etc/localtime:ro"
+		    ];
+		    ports = [
+		      "3000:3000"
+		      "222:22"
+		    ];
+		  };
 	  };
   };
-
 }
