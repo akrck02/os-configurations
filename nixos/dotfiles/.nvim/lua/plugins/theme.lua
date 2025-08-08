0b89776fec39d8672e32bdfe9bd9bb455d501737
akrck02 <aketza.vazquez@gmail.com>
2025-08-08 20:36:38 +0200

    adding lazyvim configuration, begone nvchad

---
+++ nixos/dotfiles/.nvim/lua/plugins/theme.lua
@@ -0,0 +1,28 @@
+return {
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+
+  -- Using Lazy
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = true, -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        opts = {},
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
+  },
+}
---
+++ nixos/dotfiles/.nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "typescript",
+        "vim",
+        "yaml",
+        "html",
+        "css",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      },
+    },
+  },
+  -- use mini.starter instead of alpha
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+
+  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
+  { import = "lazyvim.plugins.extras.lang.json" },
+}
