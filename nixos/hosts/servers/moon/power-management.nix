f6e9969e503749577c6a288f2b2a588dfda0a361
aketza <aketza.eguskiza@kaytek.io>
2026-05-08 22:50:20 +0200

    Update power-management.nix

--- nixos/hosts/servers/moon/power-management.nix
+++ nixos/hosts/servers/moon/power-management.nix
@@ -8,6 +8,12 @@
 		services.logind.lidSwitch = "ignore";
 		services.logind.lidSwitchExternalPower = "ignore";
 		services.logind.lidSwitchDocked = "ignore";
+
+		# Disable suspension
+		systemd.targets.sleep.enable = false;
+		systemd.targets.suspend.enable = false;
+		systemd.targets.hibernate.enable = false;
+		systemd.targets.hybrid-sleep.enable = false;
 		services.tlp = {
 		  enable = true;
 		  settings = {
