2fa30eda84897e2fc80c6d23dd308c676ade53d3
aketza <aketza.eguskiza@kaytek.io>
2026-05-08 23:12:44 +0200

    Update low.nix

--- nixos/users/low.nix
+++ nixos/users/low.nix
@@ -9,9 +9,6 @@
 
     ../modules/home-manager/development/languages/python313.nix
     ../modules/home-manager/development/languages/typescript.nix
-
-    ../modules/containers/containers/gitea.nix
-
   ];
 
   config = {
